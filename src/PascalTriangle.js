470807df52a06b4ee98ce758ac635aff5aaae309
Svitlana <svitlana.sokolova@gmail.com>
2022-05-31 19:21:01 +0200

    git push

--- src/PascalTriangle.js
+++ src/PascalTriangle.js
@@ -1,4 +1,20 @@
 function buildPascalTriangle(rows) {
-  // TODO: write some code here
+  // // TODO: write some code here
+   let triangle = [];
+    for (let i = 1; i <= rows; i += 1){
+    triangle.push(createRow(i));
+    }
+    return triangle;
 }
 
+function createRow(n) {
+  let row = [1];
+  for (let i = 1; i < n - 1; i += 1){
+    row[i] = createRow(n - 1)[i - 1] + createRow(n - 1)[i];
+  }
+  row[n - 1] = 1;
+  return row;
+}
+console.log(createRow(1));
+console.log(createRow(5));
+console.log(buildPascalTriangle(2));
